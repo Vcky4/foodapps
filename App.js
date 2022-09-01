e65d613f2aa98c2b46a90b205d6f3eadc7b8c280
Vcky4 <okon.victor.u@gmail.com>
2022-09-01 14:43:58 +0100

    navigation

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import * as React from "react";
-import {Text} from 'react-native';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Text>Hello World</Text>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options = {
+          {headerShown: false}
+        } />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+
+
 }
---
+++ components/Home.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Text, View } from 'react-native';
+
+const Home = () => {
+    return (
+        <Text>Home!</Text>
+
+    );
+}
+
+export default Home;
