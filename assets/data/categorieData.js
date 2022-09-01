1c929c9c782fe1bf00dcf29179174402ab298585
Vcky4 <okon.victor.u@gmail.com>
2022-09-01 14:19:23 +0100

    project setup and navigation

---
+++ assets/data/categorieData.js
@@ -0,0 +1,22 @@
+const categoriesData = [
+    {
+        "id": 1,
+        image: require('../images/piza-icon.png'),
+        "title": "Pizza",
+        selected: true
+    },
+    {
+        "id": 2,
+        image: require('../images/shrimp-icon.png'),
+        "title": "Seafood",
+        selected: false
+    },
+    {
+        "id": 2,
+        image: require('../images/soda-icon.png'),
+        "title": "Soft Drinks",
+        selected: false
+    },
+]
+
+export default categoriesData;
